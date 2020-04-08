e969a7fd176f181c3d4153319a4f99dc3976f2d4
oluwaseun bello <bellomsean@gmail.com>
2020-04-08 17:07:08 +0100

    added postgresql and setup api

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+orders = [
+  {
+    'room' => 'Room 1',
+    'item' => 'Rice',
+    # 'quantity' => '2'
+  },
+  # {
+  #   'room' => 'Room 1',
+  #   'item' => 'chicken',
+  #   'quantity' => '2'
+  # },
+  # {
+  #   'room' => 'Room 2',
+  #   'item' => 'Yam',
+  #   'quantity' => '1'
+  # },
+  # {
+  #   'room' => 'Room 3',
+  #   'item' => 'Noodles',
+  #   'quantity' => '2'
+  # }
+]
+
+orders.each do |i|
+  order = Order.create(room: i['room'], item: i['item'])
+end
---
+++ spec/factories/orders.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :order do
+    room { "MyString" }
+    item { "MyString" }
+    quantity { "" }
+  end
+end
